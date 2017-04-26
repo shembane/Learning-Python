8b9d866f1ca8ef041f159f5905f181b2594adb1b
Paul Jacobson <pljcbsn@gmail.com>
2017-04-26 08:59:48 +0300

    Updated exercise 13b
    Combining arg and raw_input in a very basic script

--- ex13b.py
+++ ex13b.py
@@ -7,4 +7,7 @@ print "Your first variable is called:", first
 print "Your second variable is called:", second
 print "Your third variable is called:", third
 
+# This next line just adds a prompt for a fourth value after the original input on the command line.
+
 fourth = raw_input("What is your fourth variable? ")
+print "Your fourth variable is called:", fourth
